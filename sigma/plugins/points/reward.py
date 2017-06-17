e606e797095235810efcd2fe0d290fb0d0d5b285
gphazon <gphazon@gmail.com>
2017-06-16 20:18:43 -0400

    Update reward.py

--- sigma/plugins/points/reward.py
+++ sigma/plugins/points/reward.py
@@ -9,4 +9,4 @@ async def reward(ev, message, args):
                 if not ev.cooldown.on_cooldown(ev, message):
                     points = random.randint(3, 15)
                     ev.db.add_points(message.guild, message.author, points)
-                    ev.cooldown.set_cooldown(ev, message, 60)
+                    ev.cooldown.set_cooldown(ev, message, 1)
