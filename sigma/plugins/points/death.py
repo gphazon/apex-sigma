05a9cea98388892fc7377e8b3082f873ecd6e5d5
gphazon <gphazon@gmail.com>
2017-08-09 15:55:03 -0400

    Added comments

--- sigma/plugins/points/death.py
+++ sigma/plugins/points/death.py
@@ -4,8 +4,10 @@ from config import Currency1
 
 
 async def death(cmd, message, args):
+    # command name
     if message.mentions:
         target = message.mentions[0]
+        # @Name
     else:
         target = message.author
     point_data = cmd.db.get_points(target)
@@ -19,9 +21,15 @@ async def death(cmd, message, args):
         response = discord.Embed(color=0x1ABC9C)
         response.set_author(name=f'{target.name}\'s Currency1 Data', icon_url=user_avatar(target))
         response.add_field(name='Current Wallet', value=f'```py\n{current_pts} {Currency1}\n```')
+        # displays current amount of points for "currency #1"
         response.add_field(name='This Server', value=f'```py\n{curr_srv} {Currency1}\n```')
+        # displays amount of points in the current server the command was used in
         response.add_field(name='Total Gained', value=f'```py\n{total_pts} {Currency1}\n```')
+        # displays total amount of points gained throughout every server the bot's resided in
     else:
         response = discord.Embed(color=0x696969, title=f'🔍 I couldn\'t find {target.name} in my point database.')
+        # Player pinged does not exist
     response.set_footer(text=f'{Currency1} can be earned by being an active member of the server.')
+    # Has no points
     await message.channel.send(None, embed=response)
+# everything {currency1} is the name of given points and can be altered
